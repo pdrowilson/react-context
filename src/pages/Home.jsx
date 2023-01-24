bfe8ccc35f017fadade28d35c4b061313af704f0
Pedro Wilson <pdro.wilson@gmail.com>
2023-01-24 05:37:37 -0300

    feat: refactor context to a hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import { useContext } from 'react'
-import { CounterContext } from '../context/CounterContext'
 import ChangeCounter from '../components/ChangeCounter'
 
+import { useCounterContext } from '../hooks/useCounterContext';
+
 const Home = () => {
-  const {counter} = useContext(CounterContext);
+  const {counter} = useCounterContext()
   return (
     <div>
       <h1>Counter: <span>{counter}</span></h1>
